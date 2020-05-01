5ad959e53f8cb06c60ec3328d4a6532458e7faf5
Vitalik Moroz <vitalik.moroz2@gmail.com>
2020-05-02 00:14:33 +0300

    Updates

---
+++ precache-manifest.d24f811d1bdd03b37e32738e93b816e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54cc66c3cc7c273813b8399444166efa",
+    "url": "/cursachFrontend/index.html"
+  },
+  {
+    "revision": "ad0ee2cf8d5c4ef204ac",
+    "url": "/cursachFrontend/static/js/2.92b52478.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cursachFrontend/static/js/2.92b52478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b45b07a484e607d5b2a",
+    "url": "/cursachFrontend/static/js/main.e6d6356e.chunk.js"
+  },
+  {
+    "revision": "d10cbde5b143f7c7a7bc",
+    "url": "/cursachFrontend/static/js/runtime-main.9ff0f812.js"
+  }
+]);
